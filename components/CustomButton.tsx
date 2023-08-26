f09a2901f8dd8a22c9355e974a9e8f81e47af7f7
Adugbo Victory <adugbovictory@gmail.com>
2023-08-26 20:39:44 +0100

    types prop

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
 
-const CustomButton = () => {
-  return (
-    <div>CustomButton</div>
-  )
-}
+import Image from "next/image";
 
-export default CustomButton
+import { CustomButtonProps } from "@types";
+
+const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={false}
+    type={"button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+);
+
+export default Button;
