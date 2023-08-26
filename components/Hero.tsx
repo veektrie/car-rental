010befa3802478aade22aee7caffec74ec708e48
Adugbo Victory <adugbovictory@gmail.com>
2023-08-26 16:46:16 +0100

    bug in code

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,11 +25,11 @@ const Hero = () => {
           process.
         </p>
 
-        <CustomButton
+        {/* <CustomButton
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
-        />
+        /> */}
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
