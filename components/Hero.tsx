8483add3580aff6168f4ffdc7186eb4954be69cb
Adugbo Victory <adugbovictory@gmail.com>
2023-08-26 17:13:56 +0100

    hero section not displaying

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,18 +2,9 @@
 
 import Image from "next/image";
 
-import { CustomButton } from "@components";
 
 const Hero = () => {
-  const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  return (
+ return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
@@ -25,19 +16,9 @@ const Hero = () => {
           process.
         </p>
 
-        {/* <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
-        /> */}
+        
       </div>
-      <div className="hero__image-container">
-        <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
-        </div>
-
-        <div className="hero__image-overlay" />
-      </div>
+      
     </div>
   );
 };
