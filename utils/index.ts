bf8110016fbb85fd27ebfe1d9c1566017b9adabc
Adugbo Victory <adugbovictory@gmail.com>
2023-08-27 06:10:39 +0100

    updating search manufacturers

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ce2e1569e9msh74ad9015fdb1bc0p15ea7ajsn1ca3946e19b5',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
